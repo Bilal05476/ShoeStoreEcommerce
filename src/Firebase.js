255a4e36c9eaa8e807c42b51d665f3809d03d320
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-02-18 18:04:15 +0500

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC5ZVGG5PhhFLVY2jw3UpdYNE0NRKd3tQs",
+    apiKey: "apiKey",
     authDomain: "ecommstore-55.firebaseapp.com",
     projectId: "ecommstore-55",
     storageBucket: "ecommstore-55.appspot.com",
     messagingSenderId: "934453280015",
-    appId: "1:934453280015:web:20d824304d6a7bbe84527a",
+    appId: "appID",
     measurementId: "G-D6XK2LX3BW"
 });
 
